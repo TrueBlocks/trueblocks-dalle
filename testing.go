5e971f5a69994aa7e2b97afdb19ff63b89c6e1f9
Thomas Jay Rush <jrush@quickblocks.io>
2025-09-13 19:15:55 -0400

    Cleaning

--- testing.go
+++ testing.go
@@ -3,26 +3,10 @@
 package dalle
 
 import (
-	"errors"
 	"io"
 	"net/http"
 	"os"
 )
-
-// --- HTTP and template mocks (from prompt_test.go) ---
-type mockRoundTripper struct {
-	Resp *http.Response
-	Err  error
-}
-
-func (m *mockRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
-	return m.Resp, m.Err
-}
-
-type badReader struct{}
-
-func (badReader) Read([]byte) (int, error) { return 0, errors.New("read error") }
-func (badReader) Close() error             { return nil }
 
 var (
 	openFile     = os.OpenFile
